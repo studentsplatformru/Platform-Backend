c13acae4b0fccb4261e01eab7b746a820aaae0fc
Archie-Vian <sas-artamonov@yandex.ru>
2020-06-24 21:01:54 +0300

    added endpoint module. main page shoud be working

---
+++ endpoint/src/main/java/platformbackend/endpoint/EndpointApplication.java
@@ -0,0 +1,13 @@
+package platformbackend.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointApplication.class, args);
+    }
+
+}
---
+++ endpoint/src/test/java/platformbackend/endpoint/EndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package platformbackend.endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
