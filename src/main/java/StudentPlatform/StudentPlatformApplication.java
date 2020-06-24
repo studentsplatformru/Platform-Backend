98d6d82ce6632444ae928d8da7079f6543245ee9
Archie-Vian <57408310+Archie-Vian@users.noreply.github.com>
2020-06-24 19:00:13 +0300

    Add files via upload

---
+++ src/main/java/StudentPlatform/StudentPlatformApplication.java
@@ -0,0 +1,13 @@
+package StudentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPlatformApplication.class, args);
+	}
+
+}
