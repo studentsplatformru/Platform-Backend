6bbcce1c122e55e2466ef53051895a33b01215e3
Archie-Vian <sas-artamonov@yandex.ru>
2020-07-04 16:02:08 +0300

    Правки в finder, добавлен файл конфигурации со ссылкой на расписание spbu.

--- service/src/main/java/ru/platformbackend/service/parsers/spbu/SpbuScheduleFinder.java
+++ service/src/main/java/ru/platformbackend/service/parsers/spbu/SpbuScheduleFinder.java
@@ -5,15 +5,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.Properties;
 
 
 public class SpbuScheduleFinder {
-    private String baseUrl = "https://timetable.spbu.ru/";
-    private String tempUrl = baseUrl;
+
+    private String baseUrl;
+    private String tempUrl;
     private String resultUrl;
     private Document document;
 
+    /**
+     * Метод выполняет несколько шагов, включающих переход по вебстраницам, в поисках ссылки на расписание.
+     * @param studyName Название направления подготовки на английском.
+     * @param groupName Полное имя группы.
+     * @return Ссылка на расписание конкретной группы.
+     */
     public String findScheduleLink(String studyName, String groupName) {
+        loadProperties();
         setUpDocument();
 
         findFieldOfStudy(studyName);
@@ -26,6 +35,23 @@ public class SpbuScheduleFinder {
         return resultUrl;
     }
 
+    /**
+     * Присваивание значений полям baseUrl и tempUrl на основе значения поля baseUrl в файле config.yml.
+     */
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getResourceAsStream("/config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        baseUrl = properties.getProperty("baseUrl");
+        tempUrl = baseUrl;
+    }
+
+    /**
+     * Метод, загружающий веб-страницу по значению поля tempUrl.
+     */
     private void setUpDocument() {
         try {
             document = Jsoup.connect(tempUrl).get();
@@ -34,6 +60,10 @@ public class SpbuScheduleFinder {
         }
     }
 
+    /**
+     * Метод обнаруживает ссылку на страницу, соответствующую направлению подготовки, и присваивает её полю tempUrl.
+     * @param studyName Название направления подготовки на английском.
+     */
     private void findFieldOfStudy(String studyName) {
         Elements studyFields = document.select("li[class='list-group-item']");
         for (Element fieldOfStudy : studyFields) {
@@ -43,18 +73,27 @@ public class SpbuScheduleFinder {
         }
     }
 
+    /**
+     * Метод обнаруживает ссылку на форму обучения, соответствующую имени группы, и присваивает её полю tempUrl.
+     * @param groupName Полное имя группы.
+     */
     private void findSchedulesOfDefiniteStudy(String groupName) {
         Element link = document.select("a[title*='" + groupName + "']").get(0);
         tempUrl = baseUrl + link.attr("href").substring(1);
     }
 
-
+    /**
+     * Метод обнаруживает ссылку на конкретное расписание, соответствующую имени группы,
+     * и, после небольшого форматирования, присваивает её полю resultUrl.
+     * @param groupName Полное имя группы.
+     */
     private void findScheduleForCurrentGroup(String groupName) {
+        final int startOfCheduleUrlInParam = 23;
         tempUrl = baseUrl + document.getElementsContainingOwnText(groupName)
                 .get(0)
                 .parent()
                 .attr("onclick")
-                .substring(23);
+                .substring(startOfCheduleUrlInParam);
         resultUrl = tempUrl.substring(0, tempUrl.length() - 1);
     }
 }
