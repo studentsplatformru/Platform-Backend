4140089e4d0ca2b84cbbbb4f8562582fd2b471ca
Archie-Vian <sas-artamonov@yandex.ru>
2020-07-10 20:51:05 +0300

    Интеграционные тесты Finder, небольшие изменения в нём.

--- service/src/main/java/ru/studentsplatform/backend/service/spbu/finder/SpbuScheduleUrlFinder.java
+++ service/src/main/java/ru/studentsplatform/backend/service/spbu/finder/SpbuScheduleUrlFinder.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 @Service
 public class SpbuScheduleUrlFinder implements ScheduleUrlFinder {
 
-    @Value("${spring.parser.baseUrl}")
+    @Value("${spring.parser.baseUrl:https://timetable.spbu.ru/}")
     private String baseUrl;
 
     /**
@@ -31,9 +31,9 @@ public class SpbuScheduleUrlFinder implements ScheduleUrlFinder {
             document = HtmlDocumentBuilder.getHtmlDocument(baseUrl);
             document = findFieldOfStudy(document, studyName);
             document = findSchedulesOfDefiniteStudy(document, groupName);
-            return  findScheduleForCurrentGroup(document, groupName);
-        } catch (IOException e) {
-            e.printStackTrace();
+            return findScheduleForCurrentGroup(document, groupName);
+        } catch (IOException | NullPointerException e) {
+                //ToDo: по идее можно не обрабатывать, но под вопросом!
         }
         throw new IllegalArgumentException();
     }
@@ -45,7 +45,7 @@ public class SpbuScheduleUrlFinder implements ScheduleUrlFinder {
      * @param studyName Название направления подготовки на английском.
      * @return Документ, соответствующий ссылке на страницу со списком форм обученя конкретного направления.
      */
-    private Document findFieldOfStudy(Document htmlDoc, String studyName) throws IOException {
+    private Document findFieldOfStudy(Document htmlDoc, String studyName) throws IOException, NullPointerException {
         return HtmlDocumentBuilder.getHtmlDocument(
                 baseUrl +
                         htmlDoc.getElementsContainingOwnText(studyName)
@@ -61,7 +61,7 @@ public class SpbuScheduleUrlFinder implements ScheduleUrlFinder {
      * @param groupName Полное имя группы.
      * @return Документ, соответствующий ссылке со списком групп на конкретной форме обучения.
      */
-    private Document findSchedulesOfDefiniteStudy(Document htmlDoc, String groupName) throws IOException {
+    private Document findSchedulesOfDefiniteStudy(Document htmlDoc, String groupName) throws IOException, NullPointerException {
         Element link = htmlDoc.select("a[title*='" + groupName + "']").first();
         return HtmlDocumentBuilder.getHtmlDocument(baseUrl + link.attr("href").substring(1));
     }
@@ -74,7 +74,7 @@ public class SpbuScheduleUrlFinder implements ScheduleUrlFinder {
      * @param groupName Полное имя группы.
      * @return Ссылка на страницу с расписанием для конкретной группы.
      */
-    private String findScheduleForCurrentGroup(Document htmlDoc, String groupName) {
+    private String findScheduleForCurrentGroup(Document htmlDoc, String groupName) throws NullPointerException {
         final int startOfScheduleUrlInParam = 23;
         String tempUrl = baseUrl + htmlDoc.getElementsContainingOwnText(groupName)
                 .first()
