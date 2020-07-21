c376dd0c5731c410cb68593f8249b9acc3c59b50
Alisa Khomenko <alisa.homenko@gmail.com>
2020-07-21 18:27:43 +0300

    Added LessonDTO and LessonUnitDTO

---
+++ domain/src/main/java/ru/studentsplatform/backend/dto/LessonDTO.java
@@ -0,0 +1,26 @@
+package ru.studentsplatform.backend.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import ru.studentsplatform.backend.entities.model.*;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class LessonDTO extends BaseDTO implements Serializable {
+
+    private OffsetDateTime date;
+    private List<Homework> homeworkList;
+    private List<Mark> marks;
+    private List<Attendance> attendanceList;
+    private Team team;
+    private LessonUnit lessonUnit;
+
+}
---
+++ domain/src/main/java/ru/studentsplatform/backend/dto/LessonUnitDTO.java
@@ -0,0 +1,28 @@
+package ru.studentsplatform.backend.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import ru.studentsplatform.backend.entities.model.*;
+
+import java.io.Serializable;
+import java.time.OffsetTime;
+import java.util.List;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class LessonUnitDTO extends BaseDTO implements Serializable {
+
+    private OffsetTime startTime;
+    private OffsetTime endTime;
+    private String audience;
+    private String type;
+    private String note;
+    private List<Lesson> lessons;
+    private Teacher teacher;
+    private Subject subject;
+
+}
