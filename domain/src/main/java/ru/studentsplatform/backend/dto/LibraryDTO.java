ca4d46bdd699a710e7d81ab3e04092f8264979eb
Alisa Khomenko <alisa.homenko@gmail.com>
2020-07-21 18:41:13 +0300

    Added LibraryDTO and MarkDTO

---
+++ domain/src/main/java/ru/studentsplatform/backend/dto/LibraryDTO.java
@@ -0,0 +1,20 @@
+package ru.studentsplatform.backend.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import ru.studentsplatform.backend.entities.model.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class LibraryDTO extends BaseDTO implements Serializable {
+
+    private University university;
+    private List<Material> materials;
+}
---
+++ domain/src/main/java/ru/studentsplatform/backend/dto/MarkDTO.java
@@ -0,0 +1,20 @@
+package ru.studentsplatform.backend.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import ru.studentsplatform.backend.entities.model.*;
+
+import java.io.Serializable;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class MarkDTO extends BaseDTO implements Serializable {
+
+    private String markValue;
+    private Student student;
+    private Lesson lesson;
+}
