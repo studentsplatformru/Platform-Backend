acffa50bc46bb9df0e21a2604542962366d74aa3
Alisa Khomenko <alisa.homenko@gmail.com>
2020-07-21 19:24:14 +0300

    Added StudentCouncil

--- domain/src/main/java/ru/studentsplatform/backend/dto/FacultyDTO.java
+++ domain/src/main/java/ru/studentsplatform/backend/dto/FacultyDTO.java
@@ -2,10 +2,7 @@ package ru.studentsplatform.backend.dto;
 
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import ru.studentsplatform.backend.entities.model.Department;
-import ru.studentsplatform.backend.entities.model.Direction;
-import ru.studentsplatform.backend.entities.model.JobAd;
-import ru.studentsplatform.backend.entities.model.University;
+import ru.studentsplatform.backend.entities.model.*;
 
 import java.io.Serializable;
 import java.util.List;
@@ -22,6 +19,7 @@ public class FacultyDTO extends BaseDTO implements Serializable {
     private String facultyName;
     private University university;
     private Set<Department> departments;
+    private Set<StudentCouncil> studentCouncils;
     private List<JobAd> jobAds;
     private Set<Direction> directions;
 }
