72aec70f6046d943a1ea3a76410e92b5b3730e17
Archie-Vian <sas-artamonov@yandex.ru>
2020-07-21 16:14:23 +0300

    Добавлен mapper для attendance.

--- domain/src/main/java/ru/studentsplatform/backend/mapper/AttendanceMapper.java
+++ domain/src/main/java/ru/studentsplatform/backend/mapper/AttendanceMapper.java
@@ -1,7 +1,28 @@
 package ru.studentsplatform.backend.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import ru.studentsplatform.backend.dto.AttendanceDTO;
+import ru.studentsplatform.backend.entities.model.Attendance;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AttendanceMapper {
+    @Mappings({
+            @Mapping(target = "id", source = "entity.id"),
+            @Mapping(target = "createdBy", source = "entity.createdBy"),
+            @Mapping(target = "modifiedBy", source = "entity.modifiedBy"),
+            @Mapping(target = "presence", source = "entity.presence"),
+            @Mapping(target = "lesson", source = "entity.lesson")
+    })
+    AttendanceDTO attendanceToAttendanceDTO(Attendance entity);
+
+    @Mappings({
+            @Mapping(target = "id", source = "dto.id"),
+            @Mapping(target = "createdBy", source = "dto.createdBy"),
+            @Mapping(target = "modifiedBy", source = "dto.modifiedBy"),
+            @Mapping(target = "presence", source = "dto.presence"),
+            @Mapping(target = "lesson", source = "dto.lesson")
+    })
+    Attendance attendanceDTOtoAttendance(AttendanceDTO dto);
 }
