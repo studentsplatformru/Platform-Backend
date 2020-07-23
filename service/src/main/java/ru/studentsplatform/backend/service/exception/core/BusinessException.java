0023499b84dfa62be7cf68d6d5f22e7083a1473e
Danila <karnacevich5323537@gmail.com>
2020-07-23 09:42:10 +0300

    Add business exception pattern

---
+++ service/src/main/java/ru/studentsplatform/backend/service/exception/core/BusinessException.java
@@ -0,0 +1,57 @@
+package ru.studentsplatform.backend.service.exception.core;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Objects;
+
+/**
+ * Исключения для бизнес ошибок.
+ * @author Danila K (karnacevich5323537@gmail.com) (10.07.2020).
+ */
+public class BusinessException extends RuntimeException {
+
+    /**
+     * Причина возникновения исключения
+     */
+    private final BusinessExceptionReason reason;
+
+    /**
+     * Параметры исключения
+     */
+    private final Object[] parameters;
+
+    public BusinessException(BusinessExceptionReason reason, Object... parameters) {
+        this.reason = reason;
+        this.parameters = parameters;
+    }
+
+    public BusinessExceptionReason getReason() {
+        return reason;
+    }
+
+    public Object[] getParameters() {
+        return parameters;
+    }
+
+    public String getCode() {
+        return reason.getCode();
+    }
+
+    public HttpStatus getStatus() {
+        return reason.getStatus();
+    }
+
+    /**
+     * Проверяем есть ли параметры, если нет, то возвращаем базовый текст
+     * иначе вставляем параметры.
+     *
+     * @return текст ошибки
+     */
+    @Override
+    public String getMessage() {
+        return Objects.isNull(parameters)
+                ? reason.getMessagePattern()
+                : String.format(reason.getMessagePattern(), parameters);
+    }
+
+}
---
+++ service/src/main/java/ru/studentsplatform/backend/service/exception/core/BusinessExceptionReason.java
@@ -0,0 +1,33 @@
+package ru.studentsplatform.backend.service.exception.core;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Интерфейс для описания причины возникновения ошибки
+ *
+ * @author Danila K (karnacevich5323537@gmail.com) (23.07.2020).
+ */
+public interface BusinessExceptionReason {
+
+    /**
+     * Код ошибки
+     *
+     * @return код ошибки
+     */
+    String getCode();
+
+    /**
+     * Паттерн для формирования сообщения ошибки.
+     * Подстановка текста осуществляется через замену '%s' на нужный текст.
+     *
+     * @return паттерн для формирования сообщения ошибки
+     */
+    String getMessagePattern();
+
+    /**
+     * Возвращает статус ошибки исключения
+     *
+     * @return статус ошибки
+     */
+    HttpStatus getStatus();
+}
