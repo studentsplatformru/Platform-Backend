bd5e20a9711e1d8d06a6addc485c8a89cfc3c489
Archie-Vian <sas-artamonov@yandex.ru>
2020-07-28 23:21:22 +0300

    Обновлен контроллер: Изменены пути методов контроллеров, добавлена возможность получить все task, принадлежащие к определенной ячейке. Всвязи с этим в репозиторий добавлены 2 метода поиска: по userCellId и userId. Всвязи с этим обновлен класс и интерфейс сервиса Task. Всвязи с этим обновлены тесты класса Task. Все аннотации @Transactional вынесены в контроллер. Обновлен файл api.md

--- endpoint/src/main/java/ru/studentsplatform/backend/endpoint/rest/TaskController.java
+++ endpoint/src/main/java/ru/studentsplatform/backend/endpoint/rest/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ru.studentsplatform.backend.domain.dto.TaskDTO;
 import ru.studentsplatform.backend.endpoint.mapper.TaskMapper;
+import ru.studentsplatform.backend.entities.model.university.Task;
 import ru.studentsplatform.backend.service.crud.TaskAttachmentService;
 import ru.studentsplatform.backend.service.crud.impl.TaskServiceImpl;
 import ru.studentsplatform.backend.system.annotation.Profiled;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Контроллер, служащий для создания задач для студентов.
@@ -29,7 +32,7 @@ import java.util.Arrays;
  */
 @Profiled
 @RestController
-@RequestMapping("/task")
+@RequestMapping
 public class TaskController {
 	private final TaskMapper taskMapper;
 
@@ -52,30 +55,77 @@ public class TaskController {
 	}
 
 	/**
+	 *  Возвращает сведения о задаче с выбраным Id.
+	 * @param userId Id пользователя, которому принадлежит задача
+	 * @param cellId Id ячейки расписания пользователя, к которой прикреплена задача
+	 * @param taskId Id задачи студента
+	 * @return Ответ с кодом 200(ok), содержащий сведения о задаче
+	 */
+	@Transactional
+	@GetMapping("user/{userId}/schedule/cell/{cellId}/task/{taskId}")
+	public ResponseEntity<TaskDTO> gatTask(@PathVariable(name = "userId") Long userId,
+										   @PathVariable(name = "cellId") Long cellId,
+										   @PathVariable(name = "taskId") Long taskId) {
+
+		Task task = taskService.getById(taskId);
+		TaskDTO result = taskMapper.taskToTaskDTO(task);
+
+		return ResponseEntity.ok(result);
+	}
+
+	/**
+	 * Возвращает сведения о всех задачах, прикрепленных к ячейке расписания пользователя.
+	 * @param userId Id пользователя, которому принадлежат задачи
+	 * @param cellId Id ячейки расписания пользователя, к которой прикреплены задачи
+	 * @return Ответ с кодом 200(ok), содержащий сведения о всех задачах, закрепленных за ячейкой
+	 */
+	@Transactional
+	@GetMapping("user/{userId}/schedule/cell/{cellId}/tasks")
+	public ResponseEntity<List<TaskDTO>> getAllTasks(@PathVariable(name = "userId") Long userId,
+													 @PathVariable(name = "cellId") Long cellId) {
+
+		List<Task> taskList = taskService.getByUserCell(cellId);
+		List<TaskDTO> result = taskMapper.listTaskToTaskDTO(taskList);
+
+		return ResponseEntity.ok(result);
+	}
+
+	/**
 	 * Создает на основе полученных данных объект студенчекой задачи.
 	 *
+	 * @param userId Id пользователя, которому будет принадлежать задача
+	 * @param cellId Id ячейки расписания пользователя, к которой бует прикреплена задача
 	 * @param dto Объект, содержащий данные, полученные от пользователя.
 	 * @return Ответ со статусом 200(ok), содержащий сведения о сохраненной сущности.
 	 */
-	@PostMapping
-	public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO dto) {
+	@Transactional
+	@PostMapping("user/{userId}/schedule/cell/{cellId}/task")
+	public ResponseEntity<TaskDTO> createTask(@PathVariable(name = "userId") Long userId,
+											  @PathVariable(name = "cellId") Long cellId,
+											  @RequestBody TaskDTO dto) {
+
 		var task = taskMapper.taskDTOToTask(dto);
 		task = taskService.create(task);
-
 		var result = taskMapper.taskToTaskDTO(task);
+
 		return ResponseEntity.ok(result);
 	}
 
 	/**
 	 * Позволяет прикреплять к студенческой задаче файлы с решением.
 	 *
+	 * @param userId Id пользователя, которому принадлежит задача
+	 * @param cellId Id ячейки расписания пользователя, к которой прикреплена задача
 	 * @param taskId Идентификатор задачи, к которой будет прикреплено решение.
 	 * @param files   Файлы, которые будут прикреплены к залдаче
 	 * @return Ответ со статусом 200(ok),
 	 * содержащий сведения о том, удачно ли прикреплены все полученные файлы.
 	 */
-	@PostMapping("/{id}/file")
-	public ResponseEntity<Boolean> taskAddFiles(@PathVariable(name = "id") Long taskId,
+	@Transactional
+	@PostMapping("user/{userId}/schedule/cell/{cellId}/task/{taskId}/file")
+	public ResponseEntity<Boolean> taskAddFiles(@PathVariable(name = "userId") Long userId,
+												@PathVariable(name = "cellId") Long cellId,
+												@PathVariable(name = "taskId") Long taskId,
 												@RequestParam(name = "file") MultipartFile... files) {
 
 		var result = taskService.addFilesForTask(taskId, Arrays.asList(files));
@@ -85,12 +135,17 @@ public class TaskController {
 
 	/**
 	 * Загружает выбранный файл на компьютер.
+	 * @param userId Id пользователя, которому принадлежит задача
+	 * @param cellId Id ячейки расписания пользователя, к которой прикреплена задача
 	 * @param taskId Id задачи, к которой прикреплен файл для загрузки
 	 * @param fileId Id файла дял загрузки
 	 * @return тело веб-страницы
 	 */
-	@GetMapping("/{id}/file/{fileId}")
-	public ResponseEntity<Resource> getFileRelatedToTask(@PathVariable(name = "id") Long taskId,
+	@Transactional
+	@GetMapping("user/{userId}/schedule/cell/{cellId}/task/{taskId}/file/{fileId}")
+	public ResponseEntity<Resource> getFileRelatedToTask(@PathVariable(name = "userId") Long userId,
+														 @PathVariable(name = "cellId") Long cellId,
+														 @PathVariable(name = "taskId") Long taskId,
 														 @PathVariable(name = "fileId") Long fileId) {
 
 		var file = taskAttachmentService.getByFileId(taskId, fileId);
