13d9a1423d51cf2ce7edb9a34b1ee63c98e91ddd
Archie-Vian <sas-artamonov@yandex.ru>
2020-08-01 22:00:57 +0300

    Завершены тестовые классы мапперов Discipline и Faculty

---
+++ endpoint/src/test/java/ru/studentsplatform/backend/endpoint/mapper/DisciplineMapperTest.java
@@ -0,0 +1,85 @@
+package ru.studentsplatform.backend.endpoint.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.studentsplatform.backend.domain.dto.university.DisciplineDTO;
+import ru.studentsplatform.backend.entities.model.university.Discipline;
+import ru.studentsplatform.backend.entities.model.university.Subject;
+import ru.studentsplatform.backend.entities.model.user.User;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class DisciplineMapperTest {
+
+    @Mock
+    ScheduleUserCellMapper userCellMapper;
+
+    @InjectMocks
+    DisciplineMapper mapper = new DisciplineMapperImpl();
+
+    @Test
+    void disciplineToDisciplineDTOTest(){
+        Discipline entity = new Discipline();
+        User user = new User();
+        Subject subject = new Subject();
+        subject.setId(3L);
+        user.setId(2L);
+        entity.setId(1L);
+        entity.setSemester(1);
+        entity.setCreatedBy("test");
+        entity.setUser(user);
+        entity.setSubject(subject);
+        DisciplineDTO dto = mapper.disciplineToDisciplineDTO(entity);
+        assertEquals(dto.getId(), entity.getId());
+        assertEquals(dto.getSemester(), entity.getSemester());
+        assertEquals(dto.getUserId(), entity.getUser().getId());
+        assertEquals(dto.getSubjectId(), entity.getSubject().getId());
+
+    }
+
+    @Test
+    void disciplineDTOToDisciplineTest(){
+        DisciplineDTO dto = new DisciplineDTO();
+        dto.setId(1L);
+        dto.setSemester(2);
+        dto.setSubjectId(3L);
+        dto.setUserId(4L);
+        dto.setCreatedBy("test");
+        Discipline entity = mapper.disciplineDTOToDiscipline(dto);
+        assertEquals(dto.getId(), entity.getId());
+        assertEquals(dto.getSemester(), entity.getSemester());
+        assertEquals(dto.getUserId(), entity.getUser().getId());
+        assertEquals(dto.getSubjectId(), entity.getSubject().getId());
+    }
+
+    @Test
+    void listDisciplineToDisciplineDTOTest(){
+        List<Discipline> disciplineList = new LinkedList<>();
+        Discipline testDiscipline = new Discipline();
+        testDiscipline.setId(3L);
+        disciplineList.add(testDiscipline);
+        disciplineList.add(testDiscipline);
+        List<DisciplineDTO> disciplineDTOS = mapper.listDisciplineToDisciplineDTO(disciplineList);
+        assertEquals(3L, disciplineDTOS.get(0).getId());
+        assertEquals(3L, disciplineDTOS.get(1).getId());
+    }
+
+    @Test
+    void listDisciplineDTOtoDisciplineTest() {
+        List<DisciplineDTO> disciplineDTOS = new LinkedList<>();
+        DisciplineDTO testDisciplineDTO = new DisciplineDTO();
+        testDisciplineDTO.setId(5L);
+        disciplineDTOS.add(testDisciplineDTO);
+        disciplineDTOS.add(testDisciplineDTO);
+        List<Discipline> disciplines = mapper.listDisciplineDTOToDiscipline(disciplineDTOS);
+        assertEquals(5L, disciplines.get(0).getId());
+        assertEquals(5L, disciplines.get(1).getId());
+    }
+}
---
+++ endpoint/src/test/java/ru/studentsplatform/backend/endpoint/mapper/FacultyMapperTest.java
@@ -0,0 +1,78 @@
+package ru.studentsplatform.backend.endpoint.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.studentsplatform.backend.domain.dto.university.FacultyDTO;
+import ru.studentsplatform.backend.entities.model.university.Faculty;
+import ru.studentsplatform.backend.entities.model.university.University;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class FacultyMapperTest {
+
+    @Mock
+    DepartmentMapper departmentMapper;
+
+    @Mock
+    DirectionMapper directionMapper;
+
+    @InjectMocks
+    FacultyMapper mapper = new FacultyMapperImpl();
+
+    @Test
+    void facultyToFacultyDTOTest(){
+        Faculty entity = new Faculty();
+        University university = new University();
+        university.setId(2L);
+        entity.setId(1L);
+        entity.setFaculty("test");
+        entity.setUniversity(university);
+        FacultyDTO dto = mapper.facultyToFacultyDTO(entity);
+        assertEquals(dto.getFaculty(), entity.getFaculty());
+        assertEquals(dto.getId(), entity.getId());
+        assertEquals(dto.getUniversityId(), entity.getUniversity().getId());
+    }
+
+    @Test
+    void facultyDTOtoFacultyTest(){
+        FacultyDTO dto = new FacultyDTO();
+        dto.setId(1L);
+        dto.setFaculty("test");
+        dto.setUniversityId(2L);
+        Faculty entity = mapper.facultyDTOToFaculty(dto);
+        assertEquals(dto.getFaculty(), entity.getFaculty());
+        assertEquals(dto.getId(), entity.getId());
+        assertEquals(dto.getUniversityId(), entity.getUniversity().getId());
+    }
+
+    @Test
+    void listFacultyToFacultyDTOTest(){
+        List<Faculty> facultyList = new LinkedList<>();
+        Faculty testFaculty = new Faculty();
+        testFaculty.setId(3L);
+        facultyList.add(testFaculty);
+        facultyList.add(testFaculty);
+        List<FacultyDTO> facultyDTOS = mapper.listFacultyToFacultyDTO(facultyList);
+        assertEquals(3L, facultyDTOS.get(0).getId());
+        assertEquals(3L, facultyDTOS.get(1).getId());
+    }
+
+    @Test
+    void listFacultyDTOtoFacultyTest() {
+        List<FacultyDTO> facultyDTOS = new LinkedList<>();
+        FacultyDTO testFacultyDTO = new FacultyDTO();
+        testFacultyDTO.setId(5L);
+        facultyDTOS.add(testFacultyDTO);
+        facultyDTOS.add(testFacultyDTO);
+        List<Faculty> facultys = mapper.listFacultyDTOToFaculty(facultyDTOS);
+        assertEquals(5L, facultys.get(0).getId());
+        assertEquals(5L, facultys.get(1).getId());
+    }
+}
