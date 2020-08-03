1a24a6f9db366675860ddf8b2bfb6af34a134cd1
Danila <karnacevich5323537@gmail.com>
2020-08-03 13:33:28 +0300

    javadoc

--- endpoint/src/main/java/ru/studentsplatform/backend/endpoint/config/CustomInfoContributor.java
+++ endpoint/src/main/java/ru/studentsplatform/backend/endpoint/config/CustomInfoContributor.java
@@ -5,6 +5,11 @@ import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+/**
+ * Настраиваемая страница для /actuator/info
+ *
+ * @author Danila K (karnacevich5323537@gmail.com) (03.08.2020).
+ */
 @Component
 public class CustomInfoContributor  implements InfoContributor {
     @Override
