7deb4abfe8339a1db6332fb311d10d609e1a136e
Krylov Sergey <kssergeyru@gmail.com>
2020-08-03 14:00:29 +0300

    Правки по ревью 2

--- endpoint/src/main/java/ru/studentsplatform/backend/endpoint/config/CustomInfoContributor.java
+++ endpoint/src/main/java/ru/studentsplatform/backend/endpoint/config/CustomInfoContributor.java
@@ -6,15 +6,15 @@ import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
 /**
- * Настраиваемая страница для /actuator/info
+ * Настраиваемая страница для /actuator/info.
  *
  * @author Danila K (karnacevich5323537@gmail.com) (03.08.2020).
  */
 @Component
-public class CustomInfoContributor  implements InfoContributor {
-    @Override
-    public void contribute(Info.Builder builder) {
-        builder.withDetail("actuator","This is custom info indicator. You can add your application data. " +
-                "You can share application persistent data from here");
-    }
+public class CustomInfoContributor implements InfoContributor {
+	@Override
+	public void contribute(Info.Builder builder) {
+		builder.withDetail("actuator", "This is custom info indicator. You can add your application data. " +
+				"You can share application persistent data from here");
+	}
 }
