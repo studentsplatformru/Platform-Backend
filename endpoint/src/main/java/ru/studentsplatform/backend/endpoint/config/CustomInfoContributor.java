8847f165c53c38ac528b993e35c92426ce55a4cc
Danila <karnacevich5323537@gmail.com>
2020-08-03 12:45:14 +0300

    fix swagger

---
+++ endpoint/src/main/java/ru/studentsplatform/backend/endpoint/config/CustomInfoContributor.java
@@ -0,0 +1,15 @@
+package ru.studentsplatform.backend.endpoint.config;
+
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomInfoContributor  implements InfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("customInfo","This is custom info indicator. You can add your application data. " +
+                "You can share application persistent data from here");
+    }
+}
