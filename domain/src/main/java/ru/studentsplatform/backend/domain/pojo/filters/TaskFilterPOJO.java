fe784cbdf52059a2ab9e224dbf863e1419b76827
Archie-Vian <sas-artamonov@yandex.ru>
2020-08-08 15:42:08 +0300

    Комментарии к классам фильтров.

--- domain/src/main/java/ru/studentsplatform/backend/domain/pojo/filters/TaskFilterPOJO.java
+++ domain/src/main/java/ru/studentsplatform/backend/domain/pojo/filters/TaskFilterPOJO.java
@@ -4,13 +4,25 @@ import lombok.Data;
 
 import java.time.OffsetDateTime;
 
+/**
+ * Сущность, содержащая фильтры для поиска записей Task,
+ * фильтры задаются в requestBody с названиями, соответствующими названиям полей.
+ * Поля, содержащие NULL, будут игнорироваться при поиске.
+ */
 @Data
 public class TaskFilterPOJO {
+	//Поиск с учетом временных рамок (начало)
 	private OffsetDateTime startTime;
+	//Поиск с учетом временных рамок (конец)
 	private OffsetDateTime endTime;
+	//Поиск с учетом Id предмета
 	private Long subjectId;
+	//Поиск с учетом Id группы студентов
 	private Long groupId;
+	//Поиск с учетом Id ячейки расписания пользователя
 	private Long userCellId;
+	//Поиск с учетом семестра
 	private Integer semester;
+	//Поиск с учетом Id пользователя
 	private Long userId;
 }
