1ebceee6266dced9537204124db3dc21281c4f11
Danila <karnacevich5323537@gmail.com>
2020-08-15 13:44:12 +0300

    bot template test

---
+++ notification/src/test/java/ru/studentsplatform/backend/notification/BotTemplateServiceTest.java
@@ -0,0 +1,52 @@
+package ru.studentsplatform.backend.notification;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.studentsplatform.backend.notification.enumerated.MessageType;
+import ru.studentsplatform.backend.notification.service.BotTemplateServiceImpl;
+
+/**
+ * Тесты для {@link BotTemplateService}
+ */
+@ExtendWith(MockitoExtension.class)
+public class BotTemplateServiceTest {
+
+    private BotTemplateService botTemplateService;
+
+    @BeforeEach
+    void before() {
+        botTemplateService = new BotTemplateServiceImpl();
+    }
+
+    /**
+     * Тест на получение текстового шаблона.
+     */
+    @Test
+    public void getTemplate() {
+
+        Assert.assertEquals(
+                "У Вас новая оценка!\n Test : 5",
+                botTemplateService.getBotTemplate(
+                        MessageType.MARK_NOTIFICATION,
+                        "Test", "5"));
+
+    }
+
+    /**
+     * Тест на получении ошибки при неправильном количестве аргументов.
+     */
+    @Test
+    public void getTemplateError() {
+
+        Assert.assertThrows(
+                IllegalArgumentException.class,
+                ()-> botTemplateService.getBotTemplate(
+                        MessageType.MARK_NOTIFICATION,
+                        "Test" )
+                );
+
+    }
+}
