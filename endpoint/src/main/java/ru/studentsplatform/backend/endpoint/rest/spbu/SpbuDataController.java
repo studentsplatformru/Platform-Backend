de264487423ecc48978592273b2a9103278a8cdb
Archie-Vian <sas-artamonov@yandex.ru>
2020-08-18 17:51:32 +0300

    Убран неиспользуемый импорт

--- endpoint/src/main/java/ru/studentsplatform/backend/endpoint/rest/spbu/SpbuDataController.java
+++ endpoint/src/main/java/ru/studentsplatform/backend/endpoint/rest/spbu/SpbuDataController.java
@@ -3,7 +3,6 @@ package ru.studentsplatform.backend.endpoint.rest.spbu;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.studentsplatform.backend.domain.dto.spbu.SpbuDivisionDTO;
 import ru.studentsplatform.backend.domain.dto.spbu.SpbuEventDTO;
 import ru.studentsplatform.backend.domain.dto.spbu.SpbuStudyProgramDTO;
