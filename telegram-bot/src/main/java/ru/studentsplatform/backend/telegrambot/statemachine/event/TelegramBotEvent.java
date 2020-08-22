67ac14ade3f68b72d9826a06f9f318b3ad09483b
Krylov Sergey <kssergeyru@gmail.com>
2020-08-22 18:08:29 +0300

    B-66 Fix workfloy

--- telegram-bot/src/main/java/ru/studentsplatform/backend/telegrambot/statemachine/event/TelegramBotEvent.java
+++
@@ -1,5 +0,0 @@
-package ru.studentsplatform.backend.telegrambot.statemachine.event;
-
-public enum TelegramBotEvent {
-	SET_INFO, SPBU, SPBU_GROUP, SAVE
-}
--- telegram-bot/src/main/java/ru/studentsplatform/backend/telegrambot/statemachine/state/TelegramBotState.java
+++
@@ -1,9 +0,0 @@
-package ru.studentsplatform.backend.telegrambot.statemachine.state;
-
-public enum TelegramBotState {
-	NEW,
-	CHOOSING_UNIVERSITY,
-	SPBU_SELECTED,
-	SPBU_GROUP_SELECTED,
-	END;
-}
